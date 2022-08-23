9bc510e3bb555ba423f700ef8fd73b4844816ea9
shreehari <shreeharit1@gmail.com>
2022-08-23 12:37:08 +0530

    removing image

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Continuous Integration</h1>
-     <img src="https://www.pagerduty.com/wp-content/uploads/2020/01/continuous-integration-2.png"></img>
+    
       <p>Continuous integration is a DevOps software development practice where developers regularly merge their code changes into a central repository, after which automated builds and tests are run. Continuous integration most often refers to the build or integration stage of the software release process and entails both an automation component (e.g. a CI or build service) and a cultural component (e.g. learning to integrate frequently). The key goals of continuous integration are to find and address bugs quicker, improve software quality, and reduce the time it takes to validate and release new software updates.</p>
       <hr></hr>
       <br></br>
