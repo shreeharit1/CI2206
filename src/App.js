1771d92e8a940e24b08a5010bdddf75b9857b57f
shreehari <shreeharit1@gmail.com>
2022-08-23 10:31:12 +0530

    adding another adding

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <h1>Continuous Integration</h1>
       <img src ="https://www.qentelli.com/sites/default/files/2021-05/Continuous%20Integration%201.jpg"></img>
       <p>Continuous integration is a DevOps software development practice where developers regularly merge their code changes into a central repository, after which automated builds and tests are run. Continuous integration most often refers to the build or integration stage of the software release process and entails both an automation component (e.g. a CI or build service) and a cultural component (e.g. learning to integrate frequently). The key goals of continuous integration are to find and address bugs quicker, improve software quality, and reduce the time it takes to validate and release new software updates.</p>
+      <hr></hr>
+      <br></br>
+      <h1>We are corrected some error, it should work now.</h1>
     </div>
   );
 }
